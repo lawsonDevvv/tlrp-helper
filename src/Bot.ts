e161584a7c2c57b931f8bcdd45dcc9ee4f157604
lawsonDevvv <lawson.geter@gmail.com>
2022-03-16 09:11:45 -0400

    i think i added database functionality

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,5 +1,7 @@
 import type { Message } from "discord.js";
 import { LawsonClient } from "./lib/LawsonClient";
+import { PrismaClient } from "@prisma/client";
+import { container } from "@sapphire/framework";
 // import { container } from "@sapphire/framework";
 // import { Client } from "discord-hybrid-sharding";
 
@@ -8,16 +10,21 @@ export const editsnipes = new Map<string, Message[]>();
 
 const client = new LawsonClient();
 
+export const prisma = new PrismaClient();
+
 async function main() {
   try {
     client.logger.info("Attempting to login...");
     await client.login(process.env.token ?? "0");
     client.logger.info("Successfully Logged In ✅");
+    prisma.$connect().then(() => {
+      container.logger.info("Connected to Database.")
+    });
   } catch (error) {
     client.logger.fatal(
       "I had an issue trying to initialize! The issue should be somewhere below this line."
     );
-    console.log(error);
+    console.error(error);
   }
 }
 
